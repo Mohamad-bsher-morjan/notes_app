a062e59dc0abfc11afc4eb68aa6265ead85c07a5
bsher morjan <bshermorgan4@gmail.com>
2025-09-28 14:07:08 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,48 +1,60 @@
-
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16   ),
-              child: Text(
-                'Build your carer with tharwt samy',
-                style: TextStyle(
-                  color: Colors.black.withValues(alpha: .5),
-                  fontSize: 17,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Build your carer with tharwt samy',
+                  style: TextStyle(
+                    color: Colors.black.withValues(alpha: .5),
+                    fontSize: 17,
+                  ),
                 ),
               ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete, color: Colors.black, size: 24),
+              ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete, color: Colors.black, size: 24),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May21 ,2025',
+                style: TextStyle(color: Colors.black.withValues(alpha: .5)),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May21 ,2025',
-              style: TextStyle(color: Colors.black.withValues(alpha: .5)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
