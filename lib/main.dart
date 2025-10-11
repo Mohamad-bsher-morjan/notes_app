a14fcf31e27aa94fda82c7e73fb25495ab200e86
bsher morjan <bshermorgan4@gmail.com>
2025-10-11 17:25:26 +0300

    add add note cubit to botttom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,7 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => AddNoteCubit()),
-        
-      ],
+      providers: [BlocProvider(create: (context) => AddNoteCubit())],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
